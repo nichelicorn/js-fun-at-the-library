6b152c6621115eb96c8d5252f8049b1d0c2f5000
Nichele Dunn <the7yearitch@hotmail.com>
2021-02-08 10:59:16 -0700

    Complete tests for buildMainCharacter

--- src/book.js
+++ src/book.js
@@ -1,12 +1,22 @@
 function createTitle(title) {
   return `The ${title}`;
-}
+};
+
+function buildMainCharacter(name, age, pronouns) {
+  var character =  {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  }
+
+  return character;
+};
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
+  buildMainCharacter,
   // saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
-}
+};
